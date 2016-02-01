c81f1832a70eca93a35ac15cbea665ed13f50382
Alexandre-Jacques St-Jacques <alexandre.st-jacques@groupegodin.com>
2016-02-01 11:48:31 -0500

    Ajout d'une form pour l'affichage d'un employé

---
+++ DB_Lab1/Form_AffichageEmploye.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace DB_Lab1
+{
+    partial class Form_AffichageEmploye
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_NumEmp = new System.Windows.Forms.Label();
+            this.lb_NumEmp = new System.Windows.Forms.Label();
+            this.txt_NomEmp = new System.Windows.Forms.Label();
+            this.lb_NomEmp = new System.Windows.Forms.Label();
+            this.txt_PrenomEmp = new System.Windows.Forms.Label();
+            this.lb_PrenomEmp = new System.Windows.Forms.Label();
+            this.txt_SalaireEmp = new System.Windows.Forms.Label();
+            this.lb_SalaireEmp = new System.Windows.Forms.Label();
+            this.txt_DateEmbauche = new System.Windows.Forms.Label();
+            this.lb_DateEmbauche = new System.Windows.Forms.Label();
+            this.txt_Departement = new System.Windows.Forms.Label();
+            this.lb_Departement = new System.Windows.Forms.Label();
+            this.txt_RespEmp = new System.Windows.Forms.Label();
+            this.lb_RespEmp = new System.Windows.Forms.Label();
+            this.bt_Fin = new System.Windows.Forms.Button();
+            this.bt_Suivant = new System.Windows.Forms.Button();
+            this.bt_Precedent = new System.Windows.Forms.Button();
+            this.bt_Debut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_NumEmp
+            // 
+            this.txt_NumEmp.Location = new System.Drawing.Point(44, 27);
+            this.txt_NumEmp.Name = "txt_NumEmp";
+            this.txt_NumEmp.Size = new System.Drawing.Size(100, 23);
+            this.txt_NumEmp.TabIndex = 0;
+            this.txt_NumEmp.Text = "Numéro d\'employé :";
+            this.txt_NumEmp.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_NumEmp
+            // 
+            this.lb_NumEmp.AutoSize = true;
+            this.lb_NumEmp.Location = new System.Drawing.Point(150, 27);
+            this.lb_NumEmp.Name = "lb_NumEmp";
+            this.lb_NumEmp.Size = new System.Drawing.Size(13, 13);
+            this.lb_NumEmp.TabIndex = 1;
+            this.lb_NumEmp.Text = "1";
+            // 
+            // txt_NomEmp
+            // 
+            this.txt_NomEmp.Location = new System.Drawing.Point(44, 53);
+            this.txt_NomEmp.Name = "txt_NomEmp";
+            this.txt_NomEmp.Size = new System.Drawing.Size(100, 23);
+            this.txt_NomEmp.TabIndex = 0;
+            this.txt_NomEmp.Text = "Nom :";
+            this.txt_NomEmp.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_NomEmp
+            // 
+            this.lb_NomEmp.AutoSize = true;
+            this.lb_NomEmp.Location = new System.Drawing.Point(150, 53);
+            this.lb_NomEmp.Name = "lb_NomEmp";
+            this.lb_NomEmp.Size = new System.Drawing.Size(29, 13);
+            this.lb_NomEmp.TabIndex = 1;
+            this.lb_NomEmp.Text = "Jobs";
+            // 
+            // txt_PrenomEmp
+            // 
+            this.txt_PrenomEmp.Location = new System.Drawing.Point(44, 78);
+            this.txt_PrenomEmp.Name = "txt_PrenomEmp";
+            this.txt_PrenomEmp.Size = new System.Drawing.Size(100, 23);
+            this.txt_PrenomEmp.TabIndex = 0;
+            this.txt_PrenomEmp.Text = "Prénom :";
+            this.txt_PrenomEmp.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_PrenomEmp
+            // 
+            this.lb_PrenomEmp.AutoSize = true;
+            this.lb_PrenomEmp.Location = new System.Drawing.Point(150, 78);
+            this.lb_PrenomEmp.Name = "lb_PrenomEmp";
+            this.lb_PrenomEmp.Size = new System.Drawing.Size(35, 13);
+            this.lb_PrenomEmp.TabIndex = 1;
+            this.lb_PrenomEmp.Text = "Steve";
+            // 
+            // txt_SalaireEmp
+            // 
+            this.txt_SalaireEmp.Location = new System.Drawing.Point(44, 104);
+            this.txt_SalaireEmp.Name = "txt_SalaireEmp";
+            this.txt_SalaireEmp.Size = new System.Drawing.Size(100, 23);
+            this.txt_SalaireEmp.TabIndex = 0;
+            this.txt_SalaireEmp.Text = "Salaire :";
+            this.txt_SalaireEmp.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_SalaireEmp
+            // 
+            this.lb_SalaireEmp.AutoSize = true;
+            this.lb_SalaireEmp.Location = new System.Drawing.Point(150, 104);
+            this.lb_SalaireEmp.Name = "lb_SalaireEmp";
+            this.lb_SalaireEmp.Size = new System.Drawing.Size(22, 13);
+            this.lb_SalaireEmp.TabIndex = 1;
+            this.lb_SalaireEmp.Text = "2 $";
+            // 
+            // txt_DateEmbauche
+            // 
+            this.txt_DateEmbauche.Location = new System.Drawing.Point(44, 128);
+            this.txt_DateEmbauche.Name = "txt_DateEmbauche";
+            this.txt_DateEmbauche.Size = new System.Drawing.Size(100, 23);
+            this.txt_DateEmbauche.TabIndex = 0;
+            this.txt_DateEmbauche.Text = "Date d\'embauche :";
+            this.txt_DateEmbauche.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_DateEmbauche
+            // 
+            this.lb_DateEmbauche.AutoSize = true;
+            this.lb_DateEmbauche.Location = new System.Drawing.Point(150, 128);
+            this.lb_DateEmbauche.Name = "lb_DateEmbauche";
+            this.lb_DateEmbauche.Size = new System.Drawing.Size(49, 13);
+            this.lb_DateEmbauche.TabIndex = 1;
+            this.lb_DateEmbauche.Text = "25-11-96";
+            // 
+            // txt_Departement
+            // 
+            this.txt_Departement.Location = new System.Drawing.Point(44, 151);
+            this.txt_Departement.Name = "txt_Departement";
+            this.txt_Departement.Size = new System.Drawing.Size(100, 23);
+            this.txt_Departement.TabIndex = 0;
+            this.txt_Departement.Text = "Departement :";
+            this.txt_Departement.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_Departement
+            // 
+            this.lb_Departement.AutoSize = true;
+            this.lb_Departement.Location = new System.Drawing.Point(150, 151);
+            this.lb_Departement.Name = "lb_Departement";
+            this.lb_Departement.Size = new System.Drawing.Size(17, 13);
+            this.lb_Departement.TabIndex = 1;
+            this.lb_Departement.Text = "IT";
+            // 
+            // txt_RespEmp
+            // 
+            this.txt_RespEmp.Location = new System.Drawing.Point(-45, 174);
+            this.txt_RespEmp.Name = "txt_RespEmp";
+            this.txt_RespEmp.Size = new System.Drawing.Size(189, 23);
+            this.txt_RespEmp.TabIndex = 0;
+            this.txt_RespEmp.Text = "Responsable directe :";
+            this.txt_RespEmp.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lb_RespEmp
+            // 
+            this.lb_RespEmp.AutoSize = true;
+            this.lb_RespEmp.Location = new System.Drawing.Point(150, 174);
+            this.lb_RespEmp.Name = "lb_RespEmp";
+            this.lb_RespEmp.Size = new System.Drawing.Size(64, 13);
+            this.lb_RespEmp.TabIndex = 1;
+            this.lb_RespEmp.Text = "Bob Gratton";
+            // 
+            // bt_Fin
+            // 
+            this.bt_Fin.Location = new System.Drawing.Point(189, 227);
+            this.bt_Fin.Name = "bt_Fin";
+            this.bt_Fin.Size = new System.Drawing.Size(40, 23);
+            this.bt_Fin.TabIndex = 4;
+            this.bt_Fin.Text = ">>";
+            this.bt_Fin.UseVisualStyleBackColor = true;
+            // 
+            // bt_Suivant
+            // 
+            this.bt_Suivant.Location = new System.Drawing.Point(143, 227);
+            this.bt_Suivant.Name = "bt_Suivant";
+            this.bt_Suivant.Size = new System.Drawing.Size(40, 23);
+            this.bt_Suivant.TabIndex = 4;
+            this.bt_Suivant.Text = ">";
+            this.bt_Suivant.UseVisualStyleBackColor = true;
+            // 
+            // bt_Precedent
+            // 
+            this.bt_Precedent.Location = new System.Drawing.Point(97, 227);
+            this.bt_Precedent.Name = "bt_Precedent";
+            this.bt_Precedent.Size = new System.Drawing.Size(40, 23);
+            this.bt_Precedent.TabIndex = 4;
+            this.bt_Precedent.Text = "<";
+            this.bt_Precedent.UseVisualStyleBackColor = true;
+            // 
+            // bt_Debut
+            // 
+            this.bt_Debut.Location = new System.Drawing.Point(51, 227);
+            this.bt_Debut.Name = "bt_Debut";
+            this.bt_Debut.Size = new System.Drawing.Size(40, 23);
+            this.bt_Debut.TabIndex = 4;
+            this.bt_Debut.Text = "<<";
+            this.bt_Debut.UseVisualStyleBackColor = true;
+            // 
+            // Form_AffichageEmploye
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 282);
+            this.Controls.Add(this.bt_Debut);
+            this.Controls.Add(this.bt_Precedent);
+            this.Controls.Add(this.bt_Suivant);
+            this.Controls.Add(this.bt_Fin);
+            this.Controls.Add(this.lb_RespEmp);
+            this.Controls.Add(this.txt_RespEmp);
+            this.Controls.Add(this.lb_Departement);
+            this.Controls.Add(this.txt_Departement);
+            this.Controls.Add(this.lb_DateEmbauche);
+            this.Controls.Add(this.txt_DateEmbauche);
+            this.Controls.Add(this.lb_SalaireEmp);
+            this.Controls.Add(this.txt_SalaireEmp);
+            this.Controls.Add(this.lb_PrenomEmp);
+            this.Controls.Add(this.txt_PrenomEmp);
+            this.Controls.Add(this.lb_NomEmp);
+            this.Controls.Add(this.txt_NomEmp);
+            this.Controls.Add(this.lb_NumEmp);
+            this.Controls.Add(this.txt_NumEmp);
+            this.Name = "Form_AffichageEmploye";
+            this.Text = "Details Employé";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_NumEmp;
+        private System.Windows.Forms.Label lb_NumEmp;
+        private System.Windows.Forms.Label txt_NomEmp;
+        private System.Windows.Forms.Label lb_NomEmp;
+        private System.Windows.Forms.Label txt_PrenomEmp;
+        private System.Windows.Forms.Label lb_PrenomEmp;
+        private System.Windows.Forms.Label txt_SalaireEmp;
+        private System.Windows.Forms.Label lb_SalaireEmp;
+        private System.Windows.Forms.Label txt_DateEmbauche;
+        private System.Windows.Forms.Label lb_DateEmbauche;
+        private System.Windows.Forms.Label txt_Departement;
+        private System.Windows.Forms.Label lb_Departement;
+        private System.Windows.Forms.Label txt_RespEmp;
+        private System.Windows.Forms.Button bt_Fin;
+        private System.Windows.Forms.Button bt_Suivant;
+        private System.Windows.Forms.Button bt_Precedent;
+        private System.Windows.Forms.Button bt_Debut;
+        private System.Windows.Forms.Label lb_RespEmp;
+    }
+}
---
+++ DB_Lab1/Form_AffichageEmploye.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Lab1
+{
+    public partial class Form_AffichageEmploye : Form
+    {
+        public Form_AffichageEmploye()
+        {
+            InitializeComponent();
+        }
+    }
+}
